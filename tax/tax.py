d457b39691572e8b8c9fdb12acb0a12380b723e3
Anuchit Yai-in <anuchit.im@gmail.com>
2019-05-14 17:14:06 +0700

    [Refactor] net

--- tax/tax.py
+++ tax/tax.py
@@ -1,32 +1,30 @@
+tax_rate = [
+        [0,150000], [0.05,300000], 
+        [0.1,500000], [0.15,750000], 
+        [0.2,1000000], [0.25,2000000],
+        [0.3,5000000],[0.35,9999999999.99]]
+
 def Tax(net):
-    
+    global tax_rate
     if (type(net) is not float and type(net) is not int ) or net < 0:
         return "Input Error!"
     
-    tax_rate = [0, 0.05, 0.1, 0.15, 0.2, 0.25,0.3,0.35]
-    arr_max = [150000, 300000 , 500000,750000,1000000,2000000,5000000,9999999999.99]
-    
+
+
     tax = float(0)
     net =  float(net)
 
     for i in range(1,len(tax_rate)):
-        rate = tax_rate[i]
-        _max = arr_max[i]
-        _min = arr_max[i-1]
+        rate = tax_rate[i][0]
+        _max = tax_rate[i][1]
+        _min = tax_rate[i-1][1]
         _d = net-_min
         if _d > 0:
             if _d < _max-_min :
-                #print("2 %s x %s , %s" % (rate,_d,_d))
                 tax += (rate * _d)
             else:
-                #print("3 %s x %s " % (rate,_min))
                 tax += (rate * (_min))
     return tax
-
-# tax rate 5%
-def taxRate(r,net):
-    
-    return net * r
 
 if __name__ == "__main__":
     net = float(input("Net = "))
