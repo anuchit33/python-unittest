fdd9c396670447b81fca712cc7007480ee390802
Anuchit Yai-in <anuchit.im@gmail.com>
2019-05-16 14:21:04 +0700

    poker

---
+++ poker/poker.py
@@ -0,0 +1,82 @@
+class Poker:
+    def CheckScore(arr):
+        result = 'Royal Flush'
+
+        def order(arr):
+            for i in range(0,len(arr)):
+                for j in range(0,len(arr)):
+                    if arr[i]<arr[j]:
+                        tmp = arr[i]    
+                        arr[i] = arr[j]
+                        arr[j] = tmp
+            return arr
+        def fiveofkind(arr):
+            for i in range(0,len(arr)-1):
+                if arr[i]!=arr[i+1]:
+                    return False
+            return True
+        def royal_flush(arr):
+            for i in range(0,len(arr)-1):
+                if arr[i]!=arr[i+1]-1:
+                    return False
+            return True
+        def full_house(arr):
+            b,m = group_max(arr)
+            return b==2
+        def threeofkind(arr):
+            b,m = group_max(arr)
+            return b==3 and m ==3
+        def two_pair(arr):
+            b,m = group_max(arr)
+            return b==3 and m ==2
+        def one_pair(arr):
+            b,m = group_max(arr)
+            return b==4 and m ==2
+        def group_max(arr):
+            b = {}
+            m = 0
+            for i in range(0,len(arr)):
+                k = str(arr[i])
+                b[k] = 1 if k not in b else b[k] + 1
+                m = b[k] if m < b[k] else m
+            return [len(b),m]
+
+        if royal_flush(order(arr)):
+            return 7
+        elif fiveofkind(arr): 
+            return 6
+        elif full_house(arr):
+            return 5
+        elif threeofkind(arr):
+            return 4
+        elif two_pair(arr):
+            return 3
+        elif one_pair(arr):
+            return 2
+        else:
+            return 1
+
+    def __init__(self):
+        pass
+
+    def sum(arr):
+        s = จ
+        for i in range(0,len(arr)):
+            s+=arr[i]
+
+
+    def win(result1,result2):
+        if Poker.CheckScore(result1) > Poker.CheckScore(result2):
+            return 1
+        elif Poker.CheckScore(result1) < Poker.CheckScore(result2):
+            return 2
+        elif Poker.CheckScore(result1) == Poker.CheckScore(result2):
+            if sum(result1) > sum(result2):
+                return 1
+            elif sum(result1) < sum(result2):
+                return 2
+            else:
+                return 0
+if __name__ == "__main__":
+    x = int(input("Number = "))
+    print("Poker(%s) : %s" % (x,Poker(x)))
---
+++ poker/poker_test.py
@@ -0,0 +1,65 @@
+
+import unittest
+from poker import Poker
+
+class TestPoker(unittest.TestCase):
+    def test_royal_flush(self):
+        result = [1,2,3,4,5]
+        self.assertEqual(7, Poker.CheckScore(result))
+
+    def test_royal_flush_no_order(self):
+        result = [2,6,5,4,3]
+        self.assertEqual(7, Poker.CheckScore(result))
+
+    def test_five_of_a_kind_at_1 (self):
+        result = [1,1,1,1,1]
+        self.assertEqual(6, Poker.CheckScore(result))
+
+    def test_five_of_a_kind_at_3 (self):
+        result = [3,3,3,3,3]
+        self.assertEqual(6, Poker.CheckScore(result))
+
+    def test_full_house1_2 (self):
+        result = [1,1,2,2,2]
+        self.assertEqual(5, Poker.CheckScore(result))
+
+    def test_full_house1_6 (self):
+        result = [6,1,6,1,6]
+        self.assertEqual(5, Poker.CheckScore(result))
+
+    def test_three_of_a_kind (self):
+        result = [6,1,6,2,6]
+        self.assertEqual(4, Poker.CheckScore(result))
+    def test_two_pair (self):
+        result = [3,3,1,6,6]
+        self.assertEqual(3, Poker.CheckScore(result))
+    def test_one_pair (self):
+        result = [4,2,2,3,5]
+        self.assertEqual(2, Poker.CheckScore(result))
+    
+    def test_high_card (self):
+        result = [2,3,1,4,6]
+        self.assertEqual(1, Poker.CheckScore(result))
+
+    def test_check_win_score7_vs_score1 (self):
+        result1 = [1,2,3,4,5]
+        result2 = [2,3,1,4,6]
+        self.assertEqual(1, Poker.win(result1,result2))
+
+    def test_check_win_score1_vs_score7 (self):
+        result1 = [2,3,1,4,6]
+        result2 = [1,2,3,4,5]
+        self.assertEqual(2, Poker.win(result1,result2))
+
+    def test_check_win_score7_vs_score7 (self):
+        result1 = [6,2,3,4,5]
+        result2 = [1,2,3,4,5]
+        self.assertEqual(1, Poker.win(result1,result2))
+    
+    def test_check_always(self):
+        result1 = [6,2,3,4,5]
+        result2 = [6,2,3,4,5]
+        self.assertEqual(0, Poker.win(result1,result2))
+
+if __name__ == '__main__':
+    unittest.main()
